89ca0834a45147d07da01f6a1d4cf30ebaf30cfa
Yilang Chen <ttkscyl@gmail.com>
2024-10-17 12:16:48 +0100

    initial

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<a href="task1.php">task 1</a> <br>
+<a href="task2.php">task 2</a> <br>
+<a href="task3.php">task 3</a> <br>
+<a href="task4.php">task 4</a> <br>
+<a href="task5.php">task 5</a> <br>
+<a href="task6.php">task 6</a>
+
+</body>
+</html> 
---
+++ task1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    // &emsp; makes spaces, like in java and html, &ensp; for small space
+    echo ("<br>oooooooooooo<br>o&emsp;&emsp;&emsp;&emsp;&emsp;o<br>o&emsp;#&emsp;&emsp;#&emsp;o<br>o&emsp;&ensp;&ensp;#&emsp;&emsp;&ensp;o<br>o&emsp;#&emsp;&emsp;#&emsp;o<br>o&emsp;&emsp;&emsp;&emsp;&emsp;o<br>oooooooooooo<br>");
+?>
+
+</body>
+</html> 
